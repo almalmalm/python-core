bc4cfc5b2ea20f9a66ec10ee8991f07eed50154e
almalmalm <almostkalinin@gmail.com>
2022-06-04 01:48:52 +0300

    Add print programms

---
+++ 01-print.py
@@ -0,0 +1,9 @@
+print('I\'m learning python core')
+print('My', 'name', 'is', 'Lev Kalinin')
+print('I\'m 25 years old')
+print('form kazan')
+print('I will learn', 'automated testing', 'with', 'Python!!!')
+
+print('A')
+print()
+print('B')
